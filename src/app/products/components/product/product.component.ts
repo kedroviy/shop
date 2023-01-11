a5712684d62dc5f5c106d5bbdfc32f2be19409e6
Aliaksei <Aliaksei_Medzviadkou@epam.com>
2023-01-11 21:37:23 +0300

    [Services-3]: Implement get product data from json

--- src/app/products/components/product/product.component.ts
+++ src/app/products/components/product/product.component.ts
@@ -1,10 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
-import { ProductsService } from 'src/app/products/services/products.service';
 import { ProductModel } from 'src/app/products/models/product.model';
 import { STRINGS as STR_LIST } from '../../../../app-config/const/constants';
-import { CartService } from 'src/app/cart/services/cart.service';
-
 
 @Component({
   selector: 'app-product',
@@ -12,18 +9,14 @@ import { CartService } from 'src/app/cart/services/cart.service';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  @Input() products: ProductModel[] = [];
   @Output() onBoughtProduct = new EventEmitter<ProductModel>();
-
-  productList: ProductModel[] = [];
 
   STRINGS = STR_LIST;
 
-  constructor(
-    private productService: ProductsService,
-    ) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.productList = this.productService.getProducts();
   }
 
   public onAddToCart(product: ProductModel): void {
